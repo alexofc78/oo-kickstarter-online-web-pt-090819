4d62688e3ccea647e8efa9e24159078992fb0b07
ALEXANDRA OLIVEROS <alexofc@gmail.com>
2019-10-20 15:20:28 +0000

    Automatically backed up by Learn

--- lib/project.rb
+++ lib/project.rb
@@ -9,8 +9,9 @@ class Project
     @backers = []
   end
   
-  def add_backer
-    @backers << self
+  def add_backer(backer)
+    @backers << backer
+    backer.backed_projects << self
   end
   
 end
